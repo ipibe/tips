d6d5ed681e57edb174897e2787101b95e6761b87
Pibe Aguirre <pibeaguirre9@gmail.com>
2019-01-30 14:42:05 -0800

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Pibe Aguirre on 1/30/19.
+//  Copyright © 2019 HectorTipCal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
